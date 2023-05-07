9cca3215e3f373d112e83b5b81b0749c57e3464f
Joe <josephfmck@gmail.com>
2023-05-07 00:53:49 -0500

    screw up but notes good

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 //Firebase Auth Context
 import { useAuthContext } from "../contexts/AuthContext";
 
@@ -45,6 +45,7 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
