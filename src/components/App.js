0e156d8904dec259fb25f18af0f4e4985bc61a99
Joe <josephfmck@gmail.com>
2023-05-09 15:13:56 -0500

    dashboard and signup components rendered through routes correctly

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
+import Dashboard from "./Dashboard";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
-//*browser router renamed as Router, switch determines which page currently on, Route - which page going to
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//*browser router renamed as Router, Routes determines which page currently on, Route - which page going to
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
-          <AuthProvider>    
-            <Switch>
-              <Route path="/signup" component={Signup} />
-            </Switch>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+              <Route path="/signup" element={<Signup/>} />
+            </Routes>
           </AuthProvider>
-          </Router>
-        </div>
-      </Container>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
