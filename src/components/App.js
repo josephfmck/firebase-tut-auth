f9b40a28e9f27ec23bf2a75817c1b07852bcebf5
Joe <josephfmck@gmail.com>
2023-05-09 14:58:49 -0500

    routes setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import Signup from "./Signup";
-import { Container } from "react-bootstrap"
+import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+//*browser router renamed as Router, switch determines which page currently on, Route - which page going to
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-    <div className="w-100" style={{ maxWidth: "400px"}}>
-      <Signup/>
-    </div>
-  </Container>
-    </AuthProvider>
-  )
+
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+          <AuthProvider>    
+            <Switch>
+              <Route path="/signup" component={Signup} />
+            </Switch>
+          </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+  );
 }
 
 export default App;
